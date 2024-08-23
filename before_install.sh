a05f8c606f61d287643ccd9572de780e24377b46
zawye <zawyegreenhat@gmail.com>
2024-08-23 10:24:04 +0630

    Create before_install.sh

---
+++ before_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Install node.js
+sudo apt-get install python-software-properties -y
+sudo apt-add-repository ppa:chris-lea/node.js -y
+sudo apt-get update
+sudo apt-get install nodejs -y
